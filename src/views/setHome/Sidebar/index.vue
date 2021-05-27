3fd27f9fe0c913e33020f121e5f63edd06d9646e
mwk <2515011987@qq.com>
2021-05-27 15:03:25 +0800

    首页菜单配置（拆分，封装）

---
+++ src/views/setHome/Sidebar/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-menu
+    router
+    style="border-right: none"
+    :default-active="$route.path"
+    background-color="#002033"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+    :collapse-transition="false"
+  >
+    <el-menu-item index="/">
+      <i class="el-icon-s-home"></i>
+      <span slot="title">边缘节点</span>
+    </el-menu-item>
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-chat-round"></i>
+        <span>镜像管理</span>
+      </template>
+      <el-menu-item index="/sysadmin/task">用户管理</el-menu-item>
+      <el-menu-item index="/sysadmin/result">市场镜像</el-menu-item>
+    </el-submenu>
+    <el-submenu index="3">
+      <template slot="title">
+        <i class="el-icon-chat-round"></i>
+        <span>容器管理</span>
+      </template>
+      <el-menu-item index="/sysadmin/task">容器模板</el-menu-item>
+      <el-menu-item index="/sysadmin/result">容器部署</el-menu-item>
+    </el-submenu>
+    <el-submenu index="4">
+      <template slot="title">
+        <i class="el-icon-chat-round"></i>
+        <span>云边消息</span>
+      </template>
+      <el-menu-item index="/sysadmin/task">节点配置</el-menu-item>
+      <el-menu-item index="/sysadmin/result">消息下发</el-menu-item>
+    </el-submenu>
+    <el-menu-item index="/comment">
+      <i class="el-icon-chat-dot-round"></i>
+      <span slot="title">数据分析</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+<script>
+// import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    // ...mapGetters(["routers", "sidebar"]),
+    // routes() {
+    //   // return this.routers;
+    // },
+    // isCollapse() {
+    //   return false;
+    // }
+  },
+  methods: {
+    // hasOneShowingChildren(children) {
+    //   const showingChildren = children.filter(item => {
+    //     return !item.hidden;
+    //   });
+    //   return showingChildren.length === 1;
+    // }
+  }
+};
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/views/setMain.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-container class="home-container">
+    <el-header>
+      <setHeaderTop></setHeaderTop>
+    </el-header>
+    <el-container>
+      <el-aside class="my-aside" width="270px">
+        <sidebar></sidebar>
+      </el-aside>
+      <el-main class="main_div">
+        <router-view />
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+<script>
+import setHeaderTop from "@/views/setHome/setHeaderTop";
+// import setAsideLeft from "@/pages/sysSettingMgnt/setLayout/setAsideLeft";
+import { Sidebar } from "./setHome";
+
+export default {
+  name: "Home",
+  components: {
+    setHeaderTop,
+    Sidebar
+  },
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  computed: {
+    sidebar() {}
+  }
+};
+</script>
+
+<style scoped lang="less">
+.main_div {
+  padding: 30px;
+  background: #e9effc75;
+  overflow: hidden;
+}
+.el-header[data-v-beb062de] {
+  height: 78px !important;
+  padding: 0;
+}
+.home-container {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  .my-aside {
+    width: 230px;
+    background: #002033;
+  }
+}
+</style>
